9283cdad82447fc923ba9a436a0f6aed4f701146
WouterPeere <wouterpeere@gmail.com>
2022-10-03 12:28:26 +0200

    B&W and figure 2

--- GHEtool/Articles/IEA HPT magazine 2022/IEA HPT magazine.py
+++ GHEtool/Articles/IEA HPT magazine 2022/IEA HPT magazine.py
@@ -8,6 +8,16 @@ from GHEtool import Borefield, GroundData
 import matplotlib.pyplot as plt
 import numpy as np
 
+# code for making figures black-and-white ready
+from cycler import cycler
+color_c = cycler('color', ['k'])
+style_c = cycler('linestyle', ['-', '--', ':', '-.'])
+markr_c = cycler('marker', ['', '.', 'o'])
+c_cms = color_c * markr_c * style_c
+c_csm = color_c * style_c * markr_c
+plt.rc('axes', prop_cycle=c_cms)
+plt.rcParams['lines.markersize'] = 8
+
 
 def figure_1():
     """
@@ -64,9 +74,70 @@ def figure_1():
     plt.xlabel("ln(t/ts)")
     plt.ylabel("g-function value")
     plt.ylim(-2, 60)
+
     plt.legend()
     plt.show()
 
 
+def figure_2():
+    """
+    This function creates the second figure of the article.
+    """
+
+    # initiate borefield model
+    borefield = Borefield()
+
+    # initiate depth for evaluations
+    nb_depths = 20
+    depth_array = np.linspace(50, 350, nb_depths)
+
+    # initiate list of borefield configurations
+    configs = [(10, 10), (11, 11), (12, 12), (14, 14),
+               (15, 15), (18, 18), (20, 20)]
+
+    results = []
+
+    for n1, n2 in configs:
+        depths = []
+        for H in depth_array:
+            # set ground data
+            ground_data = GroundData(H, 7, 2.4, 10, 0.12, n1, n2)
+
+            # set ground data
+            borefield.set_ground_parameters(ground_data)
+
+            # calculate gfunction
+            gfunction = borefield.gfunction(borefield.time, H)
+
+            # calculate Ra
+            Ra = (gfunction[2] - gfunction[1]) / (2 * math.pi * borefield.k_s)
+
+            # add to depths
+            depths.append(Ra)
+
+        # add to results
+        results.append(depths)
+
+    # create figure
+    plt.figure()
+    for i, config in enumerate(configs):
+        plt.plot(depth_array, results[i], label=str(config[0]) + "x" + str(config[1]))
+
+    plt.title("Ra for different borefield configurations")
+    plt.xlabel("Depth (m)")
+    plt.ylabel("Ra (mK/W)")
+
+    plt.legend()
+    plt.show()
+
+
+def figure_3():
+    """
+    This code creates the third figure of the article
+    """
+    pass
+
 if __name__ == "__main__":
-    figure_1()
+    # figure_1()
+    figure_2()
+    figure_3()
