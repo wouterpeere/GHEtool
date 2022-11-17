8898920f298709f338c71ab098b9821bb3efebd5
WouterPeere <wouterpeere@gmail.com>
2022-11-17 11:54:13 +0100

    Remove typing from Mock

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import os
 import sys
 
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'pygfunction', 'pickle',
-                'warnings', 'scipy.signal', 'math', 'os.path', 'abc', 'functools', 'typing', 'PySide6.QtCore',
+                'warnings', 'scipy.signal', 'math', 'os.path', 'abc', 'functools', 'PySide6.QtCore',
                 'PySide6.QtGui', 'PySide6.QtWidgets']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
