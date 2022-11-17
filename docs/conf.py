614c96bd20f7b7ec76f54ddf13976f81471d3eb7
WouterPeere <wouterpeere@gmail.com>
2022-11-17 11:14:17 +0100

    Fix issue autodoc and numpy

--- docs/conf.py
+++ docs/conf.py
@@ -10,8 +10,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+
+import mock
 import os
 import sys
+
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
+
 sys.path.insert(0, os.path.abspath('..'))
 import GHEtool
 
@@ -35,7 +43,8 @@ extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx_rtd_theme',
     'sphinx.ext.imgmath',
-    'numpydoc'
+    'numpydoc',
+    'sphinx.ext.autodoc'
 ]
 
 
