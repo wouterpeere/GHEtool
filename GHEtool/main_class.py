181353fa9281e54cd7f06297ea1bb0e4aead03b5
WouterPeere <wouterpeere@gmail.com>
2022-11-29 16:33:26 +0100

    Problem with example in main_class

--- GHEtool/main_class.py
+++ GHEtool/main_class.py
@@ -112,22 +112,27 @@ class Borefield:
         Examples
         --------
         monthly peak values [kW]
+
         >>> peak_cooling = np.array([0., 0, 34., 69., 133., 187., 213., 240., 160., 37., 0., 0.])
         >>> peak_heating = np.array([160., 142, 102., 55., 0., 0., 0., 0., 40.4, 85., 119., 136.])
 
         annual heating and cooling load [kWh]
+
         >>> annual_heating_load = 300 * 10 ** 3
         >>> annual_cooling_load = 160 * 10 ** 3
 
         percentage of annual load per month (15.5% for January ...)
+
         >>> monthly_load_heating_percentage = np.array([0.155, 0.148, 0.125, .099, .064, 0., 0., 0., 0.061, 0.087, 0.117, 0.144])
         >>> monthly_load_cooling_percentage = np.array([0.025, 0.05, 0.05, .05, .075, .1, .2, .2, .1, .075, .05, .025])
 
         resulting load per month [kWh]
+
         >>> monthly_load_heating = annual_heating_load * monthly_load_heating_percentage   # kWh
         >>> monthly_load_cooling = annual_cooling_load * monthly_load_cooling_percentage   # kWh
 
         create the borefield object
+
         >>> borefield = Borefield(simulation_period=20,
         >>>                      peak_heating=peak_heating,
         >>>                      peak_cooling=peak_cooling,
