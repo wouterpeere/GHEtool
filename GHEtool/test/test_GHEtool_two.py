83358cb300954c6eab6d61f8c0bca3bb399fe8e2
wouterpeere <wouter_peere@hotmail.com>
2022-12-23 11:27:40 +0100

    Increase coverage

--- GHEtool/test/test_GHEtool_two.py
+++ GHEtool/test/test_GHEtool_two.py
@@ -17,38 +17,7 @@ borefield_gt = gt.boreholes.rectangle_field(10, 12, 6, 6, 110, 4, 0.075)
 
 def load_case(number):
     """This function returns the values for one of the four cases."""
-
-    if number == 1:
-        # case 1
-        # limited in the first year by cooling
-        monthly_load_heating_percentage = np.array([0.155, 0.148, 0.125, .099, .064, 0., 0., 0., 0.061, 0.087, 0.117, 0.144])
-        monthly_load_cooling_percentage = np.array([0.025, 0.05, 0.05, .05, .075, .1, .2, .2, .1, .075, .05, .025])
-        monthly_load_heating = monthly_load_heating_percentage * 300 * 10 ** 3  # kWh
-        monthly_load_cooling = monthly_load_cooling_percentage * 150 * 10 ** 3  # kWh
-        peak_cooling = np.array([0., 0., 22., 44., 83., 117., 134., 150., 100., 23., 0., 0.])
-        peak_heating = np.zeros(12)
-
-    elif number == 2:
-        # case 2
-        # limited in the last year by cooling
-        monthly_load_heating_percentage = np.array([0.155, 0.148, 0.125, .099, .064, 0., 0., 0., 0.061, 0.087, .117, 0.144])
-        monthly_load_cooling_percentage = np.array([0.025, 0.05, 0.05, .05, .075, .1, .2, .2, .1, .075, .05, .025])
-        monthly_load_heating = monthly_load_heating_percentage * 160 * 10 ** 3  # kWh
-        monthly_load_cooling = monthly_load_cooling_percentage * 240 * 10 ** 3  # kWh
-        peak_cooling = np.array([0., 0, 34., 69., 133., 187., 213., 240., 160., 37., 0., 0.])  # Peak cooling in kW
-        peak_heating = np.array([160., 142, 102., 55., 0., 0., 0., 0., 40.4, 85., 119., 136.])
-
-    elif number == 3:
-        # case 3
-        # limited in the first year by heating
-        monthly_load_heating_percentage = np.array([0.155, 0.148, 0.125, .099, .064, 0., 0., 0., 0.061, 0.087, .117, 0.144])
-        monthly_load_cooling_percentage = np.array([0.025, 0.05, 0.05, .05, .075, .1, .2, .2, .1, .075, .05, .025])
-        monthly_load_heating = monthly_load_heating_percentage * 160 * 10 ** 3  # kWh
-        monthly_load_cooling = monthly_load_cooling_percentage * 240 * 10 ** 3  # kWh
-        peak_cooling = np.zeros(12)
-        peak_heating = np.array([300.0, 266.25, 191.25, 103.125, 0.0, 0.0, 0.0, 0.0, 75.75, 159.375, 223.125, 255.0])
-
-    else:
+    if number == 4:
         # case 4
         # limited in the last year by heating
         monthly_load_heating_percentage = np.array([0.155, 0.148, 0.125, .099, .064, 0., 0., 0., 0.061, 0.087, 0.117, 0.144])
