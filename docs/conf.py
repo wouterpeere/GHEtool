b384b2a12a46a92e3237e2473b7fc1a2d0b224e3
wouterpeere <wouter_peere@hotmail.com>
2023-01-12 11:32:33 +0100

    Make docs adaptive

--- docs/conf.py
+++ docs/conf.py
@@ -13,19 +13,25 @@
 
 import os
 import sys
+import pathlib
+import datetime
+from configparser import ConfigParser
+from GHEtool import FOLDER
 
 sys.path.insert(0, os.path.abspath('..'))
-import GHEtool
 
 # -- Project information -----------------------------------------------------
 
 project = 'GHEtool'
-copyright = '2022, Wouter Peere and Tobias Blanke'
+copyright = f'{datetime.datetime.now().year}, Wouter Peere and Tobias Blanke'
 author = 'Wouter Peere and Tobias Blanke'
 
 # The full version, including alpha/beta/rc tags
-release = '2.1.0'
-
+# get current version
+path = pathlib.Path(FOLDER).parent
+config = ConfigParser()
+config.read_file(open(path.joinpath('setup.cfg'), 'r'))
+release = config.get('metadata', 'version')
 
 # -- General configuration ---------------------------------------------------
 
