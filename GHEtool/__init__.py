4b967a212cd3007ea4b8938697912aba464a2f88
wouterpeere <wouter_peere@hotmail.com>
2023-01-14 22:00:02 +0100

    RTD fix?

--- GHEtool/__init__.py
+++ GHEtool/__init__.py
@@ -2,5 +2,5 @@ import pathlib
 
 from GHEtool.main_class import Borefield
 from GHEtool.VariableClasses import FluidData, GroundData, PipeData, CustomGFunction, GFunction
-from GHEtool.VariableClasses import SizingSetup
+from GHEtool.VariableClasses import SizingSetup, BaseClass
 FOLDER: pathlib.Path = pathlib.Path(__file__).parent  # solve problem with importing GHEtool from sub-folders
