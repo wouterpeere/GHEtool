ba0b8f669d7f482efcabee0df07697f5db7ec861
wouterpeere <wouter_peere@hotmail.com>
2023-04-07 08:38:34 +0200

    Update documentation

--- GHEtool/VariableClasses/LoadData/GeothermalLoad/MonthlyGeothermalLoadAbsolute.py
+++ GHEtool/VariableClasses/LoadData/GeothermalLoad/MonthlyGeothermalLoadAbsolute.py
@@ -8,8 +8,30 @@ from GHEtool.logger.ghe_logger import ghe_logger
 
 
 class MonthlyGeothermalLoadAbsolute(_LoadData):
+    """
+    This class contains all the information for geothermal load data with a monthly resolution and absolute input.
+    This means that the inputs are both in kWh/month and kW/month.
+    """
 
-    def __init__(self, baseload_heating, baseload_cooling, peak_heating, peak_cooling):
+    __slots__ = _LoadData.__slots__ + ('_baseload_heating', '_baseload_cooling', '_peak_heating', '_peak_cooling')
+
+    def __init__(self, baseload_heating: np.array | list | tuple = np.zeros(12),
+                 baseload_cooling: np.array | list | tuple = np.zeros(12),
+                 peak_heating: np.array | list | tuple = np.zeros(12),
+                 peak_cooling: np.array | list | tuple = np.zeros(12)):
+        """
+
+        Parameters
+        ----------
+        baseload_heating : np.array, list, tuple
+            Baseload heating values [kWh/month]
+        baseload_cooling : np.array, list, tuple
+            Baseload cooling values [kWh/month]
+        peak_heating : np.array, list, tuple
+            Peak heating values [kW/month]
+        peak_cooling : np.array, list, tuple
+            Peak cooling values [kW/month]
+        """
 
         super().__init__(hourly_resolution=False)
 
@@ -25,7 +47,23 @@ class MonthlyGeothermalLoadAbsolute(_LoadData):
         self.peak_heating = peak_heating
         self.peak_cooling = peak_cooling
 
-    def _check_input(self, input) -> bool:
+    def _check_input(self, input: np.array | list | tuple) -> bool:
+        """
+        This function checks whether the input is valid or not.
+        The input is correct if and only if:
+        1) the input is a np.array, list or tuple
+        2) the length of the input is 12
+        3) the input does not contain any negative values.
+
+        Parameters
+        ----------
+        input : np.array, list or tuple
+
+        Returns
+        -------
+        bool
+            True if the inputs are valid
+        """
         if not isinstance(input, (np.array, list, tuple)):
             ghe_logger.error("The load should be of type np.array, list or tuple.")
             return False
@@ -38,44 +76,232 @@ class MonthlyGeothermalLoadAbsolute(_LoadData):
 
     @property
     def baseload_cooling(self) -> np.array:
+        """
+        This function returns the baseload cooling in kWh/month.
+
+        Returns
+        -------
+        baseload cooling : np.array
+            Baseload cooling values for one year, so the length of the array is 12
+        """
         return self._baseload_cooling
 
     @baseload_cooling.setter
     def baseload_cooling(self, load: np.array | list | tuple) -> None:
+        """
+        This function sets the baseload cooling [kWh/month] after it has been checked.
+
+        Parameters
+        ----------
+        load : np.array, list or tuple
+            Baseload cooling [kWh/month]
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        ValueError
+            When either the length is not 12, the input is not of the correct type, or it contains negative
+            values
+        """
         if self._check_input(load):
             self._baseload_cooling = load
             return
         raise ValueError
 
+    def set_baseload_cooling(self, load: np.array | list | tuple) -> None:
+        """
+        This function sets the baseload cooling [kWh/month] after it has been checked.
+
+        Parameters
+        ----------
+        load : np.array, list or tuple
+            Baseload cooling [kWh/month]
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        ValueError
+            When either the length is not 12, the input is not of the correct type, or it contains negative
+            values
+        """
+        self.baseload_cooling = load
+
     @property
     def baseload_heating(self) -> np.array:
+        """
+        This function returns the baseload heating in kWh/month.
+
+        Returns
+        -------
+        baseload heating : np.array
+            Baseload heating values for one year, so the length of the array is 12
+        """
         return self._baseload_heating
 
     @baseload_heating.setter
-    def baseload_heating(self, load) -> None:
+    def baseload_heating(self, load: np.array | list | tuple) -> None:
+        """
+        This function sets the baseload heating [kWh/month] after it has been checked.
+
+        Parameters
+        ----------
+        load : np.array, list or tuple
+            Baseload heating [kWh/month]
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        ValueError
+            When either the length is not 12, the input is not of the correct type, or it contains negative
+            values
+        """
         if self._check_input(load):
             self._baseload_heating = load
             return
         raise ValueError
 
+    def set_baseload_heating(self, load: np.array | list | tuple) -> None:
+        """
+        This function sets the baseload heating [kWh/month] after it has been checked.
+
+        Parameters
+        ----------
+        load : np.array, list or tuple
+            Baseload heating [kWh/month]
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        ValueError
+            When either the length is not 12, the input is not of the correct type, or it contains negative
+            values
+        """
+        self.baseload_heating = load
+
     @property
     def peak_cooling(self) -> np.array:
+        """
+        This function returns the peak cooling load in kW/month.
+
+        Returns
+        -------
+        peak cooling : np.array
+            Peak cooling values for one year, so the length of the array is 12
+        """
         return self._peak_cooling
 
     @peak_cooling.setter
     def peak_cooling(self, load) -> None:
+        """
+        This function sets the peak cooling load [kW/month] after it has been checked.
+
+        Parameters
+        ----------
+        load : np.array, list or tuple
+            Peak cooling load [kW/month]
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        ValueError
+            When either the length is not 12, the input is not of the correct type, or it contains negative
+            values
+        """
         if self._check_input(load):
             self._peak_cooling = load
             return
         raise ValueError
 
+    def set_peak_cooling(self, load: np.array | list | tuple) -> None:
+        """
+        This function sets the peak cooling load [kW/month] after it has been checked.
+
+        Parameters
+        ----------
+        load : np.array, list or tuple
+            Peak cooling load [kW/month]
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        ValueError
+            When either the length is not 12, the input is not of the correct type, or it contains negative
+            values
+        """
+        self.peak_cooling = load
+
     @property
     def peak_heating(self) -> np.array:
+        """
+        This function returns the peak heating load in kW/month.
+
+        Returns
+        -------
+        peak heating : np.array
+            Peak heating values for one year, so the length of the array is 12
+        """
         return self._peak_heating
 
     @peak_heating.setter
-    def peak_heating(self, load) -> None:
+    def peak_heating(self, load: np.array | list | tuple) -> None:
+        """
+        This function sets the peak heating load [kW/month] after it has been checked.
+
+        Parameters
+        ----------
+        load : np.array, list or tuple
+            Peak heating load [kW/month]
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        ValueError
+            When either the length is not 12, the input is not of the correct type, or it contains negative
+            values
+        """
         if self._check_input(load):
             self._peak_heating = load
             return
         raise ValueError
+
+    def set_peak_heating(self, load: np.array | list | tuple) -> None:
+        """
+        This function sets the peak heating load [kW/month] after it has been checked.
+
+        Parameters
+        ----------
+        load : np.array, list or tuple
+            Peak heating load [kW/month]
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        ValueError
+            When either the length is not 12, the input is not of the correct type, or it contains negative
+            values
+        """
+        self.peak_heating = load
--- GHEtool/VariableClasses/LoadData/_LoadData.py
+++ GHEtool/VariableClasses/LoadData/_LoadData.py
@@ -16,60 +16,76 @@ class _LoadData(BaseClass, ABC):
     __slots__ = "hourly_resolution"
 
     def __init__(self, hourly_resolution: bool):
-        self.hourly_resolution: bool = hourly_resolution
-
-    @abc.abstractmethod
-    def _check_input(self, input) -> bool:
         """
 
         Parameters
         ----------
-        input
+        hourly_resolution : bool
+            True if the load class uses an hourly resolution
+        """
+        self.hourly_resolution: bool = hourly_resolution
+
+    @abc.abstractmethod
+    def _check_input(self, input: np.array | list | tuple) -> bool:
+        """
+        This function checks whether the input is valid or not.
+
+        Parameters
+        ----------
+        input : np.array, list, tuple
+            Thermal load input
 
         Returns
         -------
-
+        bool
+            True if the input is correct for the load class
         """
 
     @abc.abstractmethod
     def peak_heating(self) -> np.array:
         """
+        This function returns the peak heating load in kW/month.
 
         Returns
         -------
-
+        peak heating : np.array
         """
 
     @abc.abstractmethod
     def peak_cooling(self) -> np.array:
         """
+        This function returns the peak cooling load in kW/month.
 
         Returns
         -------
-
+        peak cooling : np.array
         """
 
     @abc.abstractmethod
     def baseload_heating(self) -> np.array:
         """
+        This function returns the baseload heating in kWh/month.
 
         Returns
         -------
-
+        baseload heating : np.array
         """
 
     @abc.abstractmethod
     def baseload_cooling(self) -> np.array:
         """
+        This function returns the baseload cooling in kWh/month.
 
         Returns
         -------
-
+        baseload cooling : np.array
         """
 
     @property
     def imbalance(self) -> float:
         """
+        This function calculates the ground imbalance.
+        A positive imbalance means that the field is injection dominated, i.e. it heats up every year.
 
         Returns
         -------
