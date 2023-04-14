8c28922a983342bc731126d5f2335715cc2bb8c8
wouterpeere <wouter_peere@hotmail.com>
2023-04-14 09:02:25 +0200

    Documentation + cost function

--- GHEtool/Examples/active_passive_cooling.py
+++ GHEtool/Examples/active_passive_cooling.py
@@ -32,7 +32,20 @@ def update_load_COP(temp_profile: np.ndarray,
                 COP:np.ndarray,
                 load_profile: np.ndarray) -> np.ndarray:
     """
-    This function updates the load profile for a given COP dependency and a temperature profile.
+    This function updates the geothermal load for heating based on a variable COP
+
+    Parameters
+    ----------
+    temp_profile : np.ndarray
+        Temperature profile of the fluid
+    COP : np.ndarray
+        Variable COP i.f.o. temperature
+    load_profile : np.ndarray
+        Heating load of the building
+
+    Returns
+    -------
+    Geothermal heating load : np.ndarray
     """
     COP_array = temp_profile * COP[0] + COP[1]
     return load_profile * (1 - 1/COP_array)
@@ -40,12 +53,79 @@ def update_load_COP(temp_profile: np.ndarray,
 
 def update_load_EER(temp_profile: np.ndarray,
                     EER: np.ndarray,
-                    threshold_active_cooling: int,
+                    threshold_active_cooling: float,
                     load_profile: np.ndarray) -> np.ndarray:
+    """
+    This function updates the geothermal load for cooling based on a threshold for active/passive cooling,
+    and a variable EER.
+
+    Parameters
+    ----------
+    temp_profile : np.ndarray
+        Temperature profile of the fluid
+    EER : np.ndarray
+        Variable EER i.f.o. temperature
+    threshold_active_cooling : float
+        Threshold of the temperature above which active cooling is needed
+    load_profile : np.ndarray
+        Cooling load of the building
+
+    Returns
+    -------
+    Geothermal cooling load : np.ndarray
+    """
     EER_array = temp_profile * EER[0] + EER[1]
     passive: np.ndarray = temp_profile < threshold_active_cooling
     active = np.invert(passive)
     return active * load_profile * (1 + 1/EER_array) + passive * load_profile
+
+
+costs = {"C_elec": 0.2159,      #  electricity cost (EUR/kWh)
+         "C_borefield": 35,     #  inv cost per m borefield (EUR/m)
+         "DR": 0.0011,         #  discount rate(-)
+         "sim_period": SIMULATION_PERIOD}
+
+
+def calculate_costs(borefield: Borefield, heating_building: np.ndarray, heating_geothermal: np.ndarray,
+                    cooling_building: np.ndarray, cooling_geothermal: np.ndarray, costs: dict) -> tuple:
+    """
+    This function calculates the relevant costs for the borefield.
+
+    Parameters
+    ----------
+    borefield : Borefield
+        Borefield object
+    heating_building : np.ndarray
+        Heating demand for the building
+    heating_geothermal : np.ndarray
+        Heating demand coming from the ground
+    cooling_building : np.ndarray
+        Cooling demand for the building
+    cooling_geothermal : np.ndarray
+        Cooling demand coming from the ground
+    costs : dict
+        Dictionary with investment cost for borefield/m, electricity cost, annual discount rate
+
+    Returns
+    -------
+    investment cost borefield, operational cost heating, operational cost cooling, total operational cost
+    """
+
+    # calculate hourly discount rate
+    DR_hourly = ((1+costs["DR"])**(1/8760) - 1)*8760
+    correction_factor = np.power(1/(1+DR_hourly), np.arange(costs["sim_period"] * 8760))
+
+    # calculate investment cost
+    investment_borefield = costs["C_borefield"] * borefield.H * borefield.number_of_boreholes
+
+    # calculate working costs
+    elec_heating = heating_building - heating_geothermal
+    elec_cooling = cooling_geothermal - cooling_building
+
+    cost_heating = elec_heating * correction_factor * costs["C_elec"]
+    cost_cooling = elec_cooling * correction_factor * costs["C_elec"]
+
+    return investment_borefield, cost_heating, cost_cooling, cost_heating+cost_cooling
 
 
 borefield = Borefield(SIMULATION_PERIOD)
