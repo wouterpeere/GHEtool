9041d268c3449c333c6f34bc0610c3b74407d79d
wouterpeere <wouter_peere@hotmail.com>
2023-05-12 17:02:03 +0200

    First structure of hourly geothermal load

--- GHEtool/VariableClasses/LoadData/GeothermalLoad/HourlyGeothermalLoad.py
+++ GHEtool/VariableClasses/LoadData/GeothermalLoad/HourlyGeothermalLoad.py
@@ -2,8 +2,153 @@
 This file contains the code for the hourly geothermal load data.
 """
 
+import numpy as np
+
+from typing import Union
 from GHEtool.VariableClasses.LoadData._LoadData import _LoadData
+from GHEtool.logger import ghe_logger
 
 
 class HourlyGeothermalLoad(_LoadData):
-    pass
+    """
+        This class contains all the information for geothermal load data with a monthly resolution and absolute input.
+        This means that the inputs are both in kWh/month and kW/month.
+        """
+
+    __slots__ = tuple(_LoadData.__slots__) + ('_heating_load', '_cooling_load')
+
+    def __init__(self, heating_load: Union[np.ndarray, list, tuple] = np.zeros(8760),
+                 cooling_load: Union[np.ndarray, list, tuple] = np.zeros(8760)):
+        """
+
+        Parameters
+        ----------
+        heating_load : np.ndarray, list, tuple
+            Heating load [kWh/h]
+        cooling_load : np.ndarray, list, tuple
+            Cooling load [kWh/h]
+        """
+
+        super().__init__(hourly_resolution=True)
+
+        # initiate variables
+        self._hourly_heating_load: np.ndarray = np.zeros(8760)
+        self._hourly_cooling_load: np.ndarray = np.zeros(8760)
+
+        # set variables
+        self.hourly_heating_load = heating_load
+        self.hourly_cooling_load = cooling_load
+
+    def _check_input(self, input: Union[np.ndarray, list, tuple]) -> bool:
+        """
+        This function checks whether the input is valid or not.
+        The input is correct if and only if:
+        1) the input is a np.ndarray, list or tuple
+        2) the length of the input is 8760
+        3) the input does not contain any negative values.
+
+        Parameters
+        ----------
+        input : np.ndarray, list or tuple
+
+        Returns
+        -------
+        bool
+            True if the inputs are valid
+        """
+        if not isinstance(input, (np.ndarray, list, tuple)):
+            ghe_logger.error("The load should be of type np.ndarray, list or tuple.")
+            return False
+        if not len(input) == 8760:
+            ghe_logger.error("The length of the load should be 12.")
+            return False
+        if np.min(input) < 0:
+            ghe_logger.error("No value in the load can be smaller than zero.")
+            return False
+        return True
+
+    @property
+    def hourly_heating_load(self) -> np.ndarray:
+        """
+        This function returns the hourly heating load in kWh/h.
+
+        Returns
+        -------
+        hourly heating : np.ndarray
+            Hourly heating values [kWh/h] for one year, so the length of the array is 8760
+        """
+        return self._hourly_heating_load
+
+    @hourly_heating_load.setter
+    def hourly_heating_load(self, load: Union[np.ndarray, list, tuple]) -> None:
+        """
+        This function sets the hourly heating load [kWh/h] after it has been checked.
+
+        Parameters
+        ----------
+        load : np.ndarray, list or tuple
+            Hourly heating [kWh/h]
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        ValueError
+            When either the length is not 8760, the input is not of the correct type, or it contains negative
+            values
+        """
+        if self._check_input(load):
+            self._hourly_heating_load = load
+            return
+        raise ValueError
+
+    @property
+    def hourly_cooling_load(self) -> np.ndarray:
+        """
+        This function returns the hourly cooling load in kWh/h.
+
+        Returns
+        -------
+        hourly cooling : np.ndarray
+            Hourly cooling values [kWh/h] for one year, so the length of the array is 8760
+        """
+        return self._hourly_cooling_load
+
+    @hourly_cooling_load.setter
+    def hourly_cooling_load(self, load: Union[np.ndarray, list, tuple]) -> None:
+        """
+        This function sets the hourly cooling load [kWh/h] after it has been checked.
+
+        Parameters
+        ----------
+        load : np.ndarray, list or tuple
+            Hourly cooling [kWh/h]
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        ValueError
+            When either the length is not 8760, the input is not of the correct type, or it contains negative
+            values
+        """
+        if self._check_input(load):
+            self._hourly_cooling_load = load
+            return
+        raise ValueError
+
+    @property
+    def imbalance(self) -> float:
+        """
+        This function calculates the ground imbalance.
+        A positive imbalance means that the field is injection dominated, i.e. it heats up every year.
+
+        Returns
+        -------
+        imbalance : float
+        """
+        return np.sum(self.hourly_cooling_load - self.hourly_heating_load)
