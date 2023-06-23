a86144eb07538476a7bbe7a5bb370b2fe40b746d
wouterpeere <wouter_peere@hotmail.com>
2023-06-23 12:40:17 +0200

    Bug fix due to overwriting + extra tests for borehole resistance

--- GHEtool/test/unit-tests/test_borehole.py
+++ GHEtool/test/unit-tests/test_borehole.py
@@ -32,7 +32,7 @@ def test_equivalent_loading():
     borehole2 = Borehole()
 
     borehole1.fluid_data = fluid_data
-    borehole1.pipe_data = pipe_data
+    borehole1.pipe_data = copy.copy(pipe_data)
 
     borehole2.pipe_data = copy.copy(pipe_data)
     borehole2.fluid_data = fluid_data
@@ -87,3 +87,17 @@ def test_calculate_Rb_no_data():
         borehole.calculate_Rb(100, 1, 0.075, 3)
     except ValueError:
         assert True
+
+def test_Rb_values():
+    borehole = Borehole()
+    mfr_range = np.arange(0.05, 0.55, 0.05)
+    pipe_data = PipeData(1, 0.015, 0.02, 0.4, 0.05, number_of_pipes=2, epsilon=1e-6)
+    borehole.pipe_data = pipe_data
+    Rb_list = []
+    for mfr in mfr_range:
+        fluid_data = FluidData(mfr, 0.568, 998, 4180, 1e-3)
+        borehole.fluid_data = fluid_data
+        Rb_list.append(borehole.get_Rb(100, 1, 0.075, 3))
+
+    assert np.array_equal(Rb_list, [0.2696064888020322, 0.16229265041543758, 0.11073836048982678, 0.09483159131195469, 0.08742765267449451, 0.08315828854931047, 0.08046372254501538, 0.07864703705590415, 0.07735912210040842, 0.07640933344353783])
+
