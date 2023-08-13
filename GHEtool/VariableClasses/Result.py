d521038bde369cedf1227378caee71a3ba6622e3
wouterpeere <wouter_peere@hotmail.com>
2023-08-13 12:33:50 +0200

    Bring up to date with Python3.11

--- GHEtool/VariableClasses/Result.py
+++ GHEtool/VariableClasses/Result.py
@@ -1,13 +1,33 @@
 """
 This file implements a Result class for temperature profiles.
 """
-from dataclasses import dataclass
 import numpy as np
 
 
-@dataclass(frozen=True)
 class Results:
-    peak_heating: np.ndarray = np.array([])
-    peak_cooling: np.ndarray = np.array([])
-    monthly_heating: np.ndarray = np.array([])
-    monthly_cooling: np.ndarray = np.array([])
+
+    def __init__(self,
+                 peak_heating: np.ndarray = np.array([]),
+                 peak_cooling: np.ndarray = np.array([]),
+                 monthly_heating: np.ndarray = np.array([]),
+                 monthly_cooling: np.ndarray = np.array([])):
+        self._peak_heating = peak_heating
+        self._peak_cooling = peak_cooling
+        self._monthly_heating = monthly_heating
+        self._monthly_cooling = monthly_cooling
+
+    @property
+    def peak_heating(self) -> np.ndarray:
+        return self._peak_heating
+
+    @property
+    def peak_cooling(self) -> np.ndarray:
+        return self._peak_cooling
+
+    @property
+    def monthly_heating(self) -> np.ndarray:
+        return self._monthly_heating
+
+    @property
+    def monthly_cooling(self) -> np.ndarray:
+        return self._monthly_cooling
