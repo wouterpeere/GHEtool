210914295e9e9d630fcd6ddc6b83049d055f2165
wouterpeere <wouter_peere@hotmail.com>
2023-10-06 11:58:31 +0200

    Bugfix

--- GHEtool/gui/test_gui/test_advanced_options.py
+++ GHEtool/gui/test_gui/test_advanced_options.py
@@ -27,6 +27,7 @@ def test_advanced_options(qtbot):
     main_window.save_scenario()
 
     gs = main_window.gui_structure
+    gs.option_source_ground_temperature.set_value(0)
     gs.option_method_rb_calc.set_value(0)
 
     assert gs.category_advanced_options.is_hidden()
