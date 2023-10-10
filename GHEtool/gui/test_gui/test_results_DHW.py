639fa70ec04e10ed75a45e3f45c0b34a1d0432ae
wouterpeere <wouter_peere@hotmail.com>
2023-10-10 11:31:06 +0200

    Final bugfix in tests

--- GHEtool/gui/test_gui/test_results_DHW.py
+++ GHEtool/gui/test_gui/test_results_DHW.py
@@ -131,5 +131,5 @@ def test_results_DHW(qtbot):
 
     main_window.display_results()
 
-    assert gs.results_heating_load.label.text() == "Heating load on the borefield: 566750.0 kWh"
+    assert gs.results_heating_load.label.text() == "Heating load on the borefield: 530633.0 kWh"
 
