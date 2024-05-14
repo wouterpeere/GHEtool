9e325ceabe974ccca3f7a6324c04c195348efdb6
wouterpeere <wouter_peere@hotmail.com>
2024-05-14 10:00:31 +0200

    Renamed main_class.py to borefield.py

--- GHEtool/__init__.py
+++ GHEtool/__init__.py
@@ -1,6 +1,6 @@
 import pathlib
 
-from GHEtool.main_class import Borefield
+from GHEtool.borefield import Borefield
 from GHEtool.VariableClasses import *
 FOLDER: pathlib.Path = pathlib.Path(__file__).parent  # solve problem with importing GHEtool from sub-folders
 from GHEtool.logger import ghe_logger
