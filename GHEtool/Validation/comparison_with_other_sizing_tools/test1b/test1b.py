7d3a4c6624ad57e7ea7d961d472478d721c23f75
wouterpeere <wouter_peere@hotmail.com>
2024-05-16 18:32:48 +0200

    Rework test_1a and test_1b

--- GHEtool/Validation/comparison_with_other_sizing_tools/test1b/test1b.py
+++ GHEtool/Validation/comparison_with_other_sizing_tools/test1b/test1b.py
@@ -13,49 +13,36 @@ comparison [in eng]. Renewable sustainable energy reviews (OXFORD) 110:247–265
 """
 # import all the relevant functions
 from GHEtool import *
-from statistics import mean
 import time
 
-if __name__ == "__main__":
 
-
+def test_1b():
     # initiate ground, fluid and pipe data
     ground_data = GroundFluxTemperature(k_s=1.8, T_g=17.5, volumetric_heat_capacity=2073600, flux=0)
     fluid_data = FluidData(mfr=0.5585, rho=1052, Cp=3795, mu=0.0052, k_f=0.48)
-    pipe_data = MultipleUTube(r_in=0.0137, r_out=0.0167, D_s=0.075/2, k_g=1.4, k_p=0.43, number_of_pipes=1)
+    pipe_data = MultipleUTube(r_in=0.0137, r_out=0.0167, D_s=0.075 / 2, k_g=1.4, k_p=0.43, number_of_pipes=1)
 
     # start test with dynamic Rb*
     # initiate borefield
     borefield = Borefield()
+
     # set ground data in borefield
     borefield.set_ground_parameters(ground_data)
     borefield.set_fluid_parameters(fluid_data)
     borefield.set_pipe_parameters(pipe_data)
     borefield.create_rectangular_borefield(1, 1, 6, 6, 110, 4, 0.075)
 
-    options = {'nSegments': 12,
-                   'segment_ratios': None,
-                   'disp': False,
-                   'profiles': True,
-                   'method': 'equivalent'
-                   }
-
-    borefield.set_options_gfunction_calculation(options)
-
     # load the hourly profile
     load = HourlyGeothermalLoad(simulation_period=10)
-    load.load_hourly_profile("test1b.csv", header=True, separator=";", decimal_seperator=",",col_heating=1, col_cooling=0)
+    load.load_hourly_profile("test1b.csv", header=True, separator=";", decimal_seperator=",", col_heating=1,
+                             col_cooling=0)
     borefield.load = load
 
-    Qmax = load.hourly_heating_load.max()
-
-    if load.hourly_cooling_load.max() > Qmax:
-        Qmax = load.hourly_cooling_load.max()
-    Dt = Qmax*1000/(fluid_data.Cp * fluid_data.mfr)
+    delta_t = max(load.max_peak_cooling, load.max_peak_cooling) * 1000 / (fluid_data.Cp * fluid_data.mfr)
 
     # set temperature bounds
-    borefield.set_max_avg_fluid_temperature(35 + Dt/2)
-    borefield.set_min_avg_fluid_temperature(0 - Dt/2)
+    borefield.set_max_avg_fluid_temperature(35 + delta_t / 2)
+    borefield.set_min_avg_fluid_temperature(0 - delta_t / 2)
 
     # Sizing with dynamic Rb
     # according to L2
@@ -84,15 +71,15 @@ if __name__ == "__main__":
     borefield.create_rectangular_borefield(1, 1, 6, 6, 110, 4, 0.075)
     Rb_static = 0.13
     borefield.set_Rb(Rb_static)
-    borefield.set_options_gfunction_calculation(options)
 
     # set temperature bounds
-    borefield.set_max_avg_fluid_temperature(35 + Dt/2)
-    borefield.set_min_avg_fluid_temperature(0 - Dt/2)
+    borefield.set_max_avg_fluid_temperature(35 + delta_t / 2)
+    borefield.set_min_avg_fluid_temperature(0 - delta_t / 2)
 
     # load the hourly profile
     load = HourlyGeothermalLoad(simulation_period=10)
-    load.load_hourly_profile("test1b.csv", header=True, separator=";", decimal_seperator=",",col_heating=1, col_cooling=0)
+    load.load_hourly_profile("test1b.csv", header=True, separator=";", decimal_seperator=",", col_heating=1,
+                             col_cooling=0)
     borefield.load = load
 
     L2s_start = time.time()
@@ -115,3 +102,7 @@ if __name__ == "__main__":
           "m (using constant Rb*)")
     print("The sizing according to L4 has a depth of", depth_L4, "m (using dynamic Rb*) and", depth_L4s,
           "m (using constant Rb*)")
+
+
+if __name__ == "__main__":
+    test_1b()
