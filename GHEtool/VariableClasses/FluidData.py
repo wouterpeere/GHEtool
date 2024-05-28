c9ccd7fadfb7654af2ad85f3fcfeb58bb6456cbd
wouterpeere <wouter_peere@hotmail.com>
2024-05-28 16:09:25 +0200

    initial commit

--- GHEtool/VariableClasses/FluidData.py
+++ GHEtool/VariableClasses/FluidData.py
@@ -13,13 +13,14 @@ class FluidData(BaseClass):
     Contains information regarding the fluid data of the borefield.
     """
 
-    __slots__ = 'k_f', 'rho', 'Cp', 'mu', 'mfr'
+    __slots__ = 'k_f', 'rho', 'Cp', 'mu', '_mfr', '_vfr'
 
     def __init__(self, mfr: float = None,
                  k_f: float = None,
                  rho: float = None,
                  Cp: float = None,
-                 mu: float = None):
+                 mu: float = None,
+                 vfr: float = None):
         """
 
         Parameters
@@ -34,12 +35,86 @@ class FluidData(BaseClass):
             Thermal capacity of the fluid [J/kgK]
         mu : float
             Dynamic viscosity of the fluid [Pa/s]
+        vfr : float
+            Volume flow rate per borehole [l/s]
         """
         self.k_f: float | None = k_f  # Thermal conductivity W/mK
-        self.mfr: float | None = mfr  # Mass flow rate per borehole kg/s
+        self._mfr: float | None = mfr  # Mass flow rate per borehole kg/s
         self.rho: float | None = rho  # Density kg/m3
         self.Cp: float | None = Cp    # Thermal capacity J/kgK
         self.mu: float | None = mu    # Dynamic viscosity Pa/s
+        self._vfr: float | None = vfr  # Volume flow rate l/s
+        
+        if self._mfr is not None and self._vfr is not None:
+            raise ValueError('You cannot set both the mass flow rate and volume flow rate')
+        else:
+            if self._mfr is None:
+                self._mfr = 0
+            else:
+                self._vfr = 0
+
+    @property
+    def vfr(self) -> float:
+        """
+        This function returns the volume flow rate.
+
+        Returns
+        -------
+        float
+            volume flow rate [l/s]
+        """
+        return self._vfr
+
+    @vfr.setter
+    def vfr(self, vfr: float) -> None:
+        """
+        This function sets the volume flow rate.
+        The mass flow rate will be set to 0
+
+        Parameters
+        ----------
+        vfr : float
+            Volume flow rate [l/s]
+
+        Returns
+        -------
+        None
+        """
+        self._vfr = vfr
+        self.mfr = 0
+
+    @property
+    def mfr(self) -> float:
+        """
+        This function returns the mass flow rate. Either based on a given mass flow rate,
+        or calculated based on a volume flow rate.
+
+        Returns
+        -------
+        float
+            mass flow rate [kg/s]
+        """
+        if self._mfr is not None:
+            return self._mfr
+        return self.vfr / 1000 * self.rho
+
+    @mfr.setter
+    def mfr(self, mfr: float) -> None:
+        """
+        This function sets the mass flow rate.
+        The vfr will hence be set to 0.
+
+        Parameters
+        ----------
+        mfr : float
+            Mass flow rate [kg/s]
+
+        Returns
+        -------
+        None
+        """
+        self._mfr = mfr
+        self.vfr = 0
 
     def set_mass_flow_rate(self, mfr: float) -> None:
         """
