03ec5d5409801e0917eb063bcae2df8cb0eb153e
wouterpeere <wouter_peere@hotmail.com>
2024-05-29 12:20:10 +0200

    Set default to None Baseclass changes

--- GHEtool/test/unit-tests/test_fluiddata.py
+++ GHEtool/test/unit-tests/test_fluiddata.py
@@ -13,7 +13,7 @@ def test_fluid_data():
     assert data.rho == 998
     assert data.Cp == 4180
     assert data.mu == 1e-3
-    assert data.vfr == 0
+    assert data.vfr is None
 
 
 def test_fluid_data_equal():
@@ -50,7 +50,7 @@ def test_set_vfr():
     with pytest.raises(ValueError):
         FluidData(0.2, 0.568, 998, 4180, 1e-3, 0.2)
     data_fluid = FluidData(0.2, 0.568, 998, 4180, 1e-3)
-    assert data_fluid.vfr == 0
+    assert data_fluid.vfr is None
     data_fluid.vfr = 0.2
     assert data_fluid._mfr == 0
     assert data_fluid.vfr == 0.2
