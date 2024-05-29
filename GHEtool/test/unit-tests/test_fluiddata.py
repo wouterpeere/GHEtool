285e9504caa5d565ee763817839b0e8722a8607d
wouterpeere <wouter_peere@hotmail.com>
2024-05-29 12:30:28 +0200

    Bugfix in test

--- GHEtool/test/unit-tests/test_fluiddata.py
+++ GHEtool/test/unit-tests/test_fluiddata.py
@@ -52,17 +52,17 @@ def test_set_vfr():
     data_fluid = FluidData(0.2, 0.568, 998, 4180, 1e-3)
     assert data_fluid.vfr is None
     data_fluid.vfr = 0.2
-    assert data_fluid._mfr == 0
+    assert data_fluid._mfr is None
     assert data_fluid.vfr == 0.2
     assert np.isclose(data_fluid.mfr, 0.2*998/1000)
 
     data_fluid.mfr = 0.2
     assert data_fluid._mfr == 0.2
-    assert data_fluid.vfr == 0
+    assert data_fluid.vfr is None
     assert data_fluid.mfr == 0.2
 
     data_fluid = FluidData(None, 0.568, 998, 4180, 1e-3, 0.2)
-    assert data_fluid._mfr == 0
+    assert data_fluid._mfr is None
     assert data_fluid.vfr == 0.2
     assert np.isclose(data_fluid.mfr, 0.2 * 998 / 1000)
 
