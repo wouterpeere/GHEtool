264e35e6729136fdd9c976acd4cf41a7720d819d
u0169319 <lone.meertens@kuleuven.be>
2024-06-13 15:31:48 +0200

    adding test cases to validate method

---
+++ GHEtool/Validation/comparison_with_other_sizing_tools/test2/test2.py
@@ -0,0 +1,121 @@
+"""
+The work of (Ahmadfard and Bernier, 2019) provides a set of test cases that can be used to compare
+software tools with the ultimate goal of improving the reliability of design methods for sizing
+vertical ground heat exchangers. This document delivers the results on the test file using the GHEtool
+L2-, L3- and L4-sizing methods.
+
+Test 2 – Shonder's test – 120 boreholes
+
+References:
+-----------
+    - Ahmadfard, M., and M. Bernier. 2019. A review of vertical ground heat exchanger sizing tools including an inter-model
+comparison [in eng]. Renewable sustainable energy reviews (OXFORD) 110:247–265.
+"""
+import os
+import time
+
+import numpy as np
+
+from GHEtool import *
+
+
+def test_2_6h():
+    # initiate ground, fluid and pipe data
+    ground_data = GroundFluxTemperature(k_s=2.25, T_g=12.41, volumetric_heat_capacity=2877000, flux=0)
+    fluid_data = FluidData(mfr=0.2416667, rho=1026, Cp=4019, mu=0.003377, k_f=0.468)
+    pipe_data = MultipleUTube(r_in=0.0137, r_out=0.0167, D_s=0.0471 / 2, k_g=1.73, k_p=0.45)
+
+    # start test with dynamic Rb*
+    # initiate borefield
+    borefield = Borefield()
+
+    # set ground data in borefield
+    borefield.set_ground_parameters(ground_data)
+    borefield.set_fluid_parameters(fluid_data)
+    borefield.set_pipe_parameters(pipe_data)
+    borefield.create_rectangular_borefield(12, 10, 6, 6, 110, 3, 0.054)
+
+    # load the hourly profile
+    load = HourlyGeothermalLoad(simulation_period=10)
+    load.load_hourly_profile(os.path.join(os.path.dirname(__file__), 'test2.csv'), header=True, separator=",",
+                             col_heating=1, col_cooling=0)
+    borefield.load = load
+
+    delta_t = max(load.max_peak_cooling, load.max_peak_cooling) * 1000 / (fluid_data.Cp * fluid_data.mfr) / 120
+
+    # set temperature bounds
+    borefield.set_max_avg_fluid_temperature(35 + delta_t / 2)
+    borefield.set_min_avg_fluid_temperature(4.4 - delta_t / 2)
+
+    # according to L2
+    L2_start = time.time()
+    depth_L2 = borefield.size(100, L2_sizing=True)
+    Rb_L2 = borefield.Rb
+    L2_stop = time.time()
+
+    # according to L3
+    L3_start = time.time()
+    depth_L3 = borefield.size(100, L3_sizing=True)
+    Rb_L3 = borefield.Rb
+    L3_stop = time.time()
+
+    # according to L4
+    L4_start = time.time()
+    depth_L4 = borefield.size(100, L4_sizing=True)
+    Rb_L4 = borefield.Rb
+    L4_stop = time.time()
+
+    # start test with constant Rb*
+    # initiate borefield
+    borefield = Borefield()
+
+    # set ground data in borefield
+    borefield.set_ground_parameters(ground_data)
+    borefield.set_fluid_parameters(fluid_data)
+    borefield.set_pipe_parameters(pipe_data)
+    borefield.create_rectangular_borefield(12, 10, 6, 6, 110, 3, 0.054)
+    Rb_static = 0.113
+    borefield.set_Rb(Rb_static)
+
+    # set temperature bounds
+    borefield.set_max_avg_fluid_temperature(35 + delta_t / 2)
+    borefield.set_min_avg_fluid_temperature(4.4 - delta_t / 2)
+
+    # load the hourly profile
+    borefield.load = load
+
+    # Sizing with constant Rb
+    L2s_start = time.time()
+    depth_L2s = borefield.size(100, L2_sizing=True)
+    L2s_stop = time.time()
+
+    # according to L3
+    L3s_start = time.time()
+    depth_L3s = borefield.size(100, L3_sizing=True)
+    L3s_stop = time.time()
+
+    # according to L4
+    L4s_start = time.time()
+    depth_L4s = borefield.size(100, L4_sizing=True)
+    L4s_stop = time.time()
+
+    print(
+        f"The sizing according to L2 has a depth of {depth_L2:.2f}m (using dynamic Rb* of {Rb_L2:.3f}) and {depth_L2s:.2f}m (using constant Rb*)")
+    print(
+        f"The sizing according to L3 has a depth of {depth_L3:.2f}m (using dynamic Rb* of {Rb_L3:.3f}) and {depth_L3s:.2f}m (using constant Rb*)")
+    print(
+        f"The sizing according to L4 has a depth of {depth_L4:.2f}m (using dynamic Rb* of {Rb_L4:.3f}) and {depth_L4s:.2f}m (using constant Rb*)")
+
+    assert np.isclose(depth_L2, 76.84063723898525)
+    assert np.isclose(depth_L3, 79.11228401910488)
+    assert np.isclose(depth_L4, 84.77559168467357)
+    assert np.isclose(depth_L2s, 77.43318625702659)
+    assert np.isclose(depth_L3s, 79.59733272530252)
+    assert np.isclose(depth_L4s, 84.97661469091508)
+    assert np.isclose(Rb_L2, 0.11113022659380956)
+    assert np.isclose(Rb_L3, 0.11146734480480838)
+    assert np.isclose(Rb_L4, 0.11234852494964061)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    test_2_6h()
