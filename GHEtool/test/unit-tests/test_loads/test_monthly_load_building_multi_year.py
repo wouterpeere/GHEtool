f7d2d024554af85689115fbcf9c58744d5cd0b4d
wouterpeere <wouter_peere@hotmail.com>
2025-04-22 16:25:53 +0200

    Fixes issue 339

--- GHEtool/test/unit-tests/test_loads/test_monthly_load_building_multi_year.py
+++ GHEtool/test/unit-tests/test_loads/test_monthly_load_building_multi_year.py
@@ -42,10 +42,34 @@ def test_checks_multiyear_monthly():
 
 
 def test_baseload_heating():
+    load = MonthlyBuildingLoadMultiYear(np.full(12, 0), np.full(12, 0), np.full(12, 0), np.full(12, 0), 10, 5)
+    assert np.allclose(load.baseload_heating, np.full(12, 0))
+    load.baseload_heating = np.linspace(0, 11, 12)
+    assert np.allclose(load.baseload_heating, np.linspace(0, 11, 12))
+    load.baseload_heating = np.linspace(1, 12, 12)
+    assert np.allclose(load.baseload_heating, np.linspace(1, 12, 12))
+    assert np.allclose(load.baseload_heating / 730, load.monthly_baseload_heating_power)
+    assert np.allclose(load.monthly_baseload_heating_power, load.peak_heating)
+    assert np.allclose(load.monthly_baseload_extraction_power_simulation_period,
+                       load.monthly_peak_extraction_simulation_period)
+    with pytest.raises(ValueError):
+        load.baseload_heating = np.ones(11)
     assert np.array_equal(load_data.baseload_heating, baseload_heating)
 
 
 def test_baseload_cooling():
+    load = MonthlyBuildingLoadMultiYear(np.full(12, 0), np.full(12, 0), np.full(12, 0), np.full(12, 0), 10, 5)
+    assert np.allclose(load.baseload_cooling, np.full(12, 0))
+    load.baseload_cooling = np.linspace(0, 11, 12)
+    assert np.allclose(load.baseload_cooling, np.linspace(0, 11, 12))
+    load.baseload_cooling = np.linspace(1, 12, 12)
+    assert np.allclose(load.baseload_cooling, np.linspace(1, 12, 12))
+    assert np.allclose(load.baseload_cooling / 730, load.monthly_baseload_cooling_power)
+    assert np.allclose(load.monthly_baseload_cooling_power, load.peak_cooling)
+    assert np.allclose(load.monthly_baseload_injection_power_simulation_period,
+                       load.monthly_peak_injection_simulation_period)
+    with pytest.raises(ValueError):
+        load.baseload_cooling = np.ones(11)
     assert np.array_equal(load_data.baseload_cooling, baseload_cooling)
 
 
