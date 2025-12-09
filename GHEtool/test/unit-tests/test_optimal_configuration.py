74ecd0b745b69542a4f57df03f889c9a07aa0370
wouterpeere <wouter_peere@hotmail.com>
2025-12-09 18:11:38 +0100

    increase coverage

--- GHEtool/test/unit-tests/test_optimal_configuration.py
+++ GHEtool/test/unit-tests/test_optimal_configuration.py
@@ -2,7 +2,7 @@ import pytest
 
 from GHEtool import *
 from GHEtool.Validation.cases import load_case
-from GHEtool.Methods.optimise_borefield_configuration import optimise_borefield_configuration
+from GHEtool.Methods.optimise_borefield_configuration import optimise_borefield_configuration, _check_identical_field
 from GHEtool.VariableClasses.BaseClass import UnsolvableOptimalFieldError
 
 
@@ -33,3 +33,15 @@ def test_equal_hmin_hmax():
     borefield.calculation_setup(use_neural_network=True)
 
     optimise_borefield_configuration(borefield, 100, 100, 5, 7, 0.5, 150, 150, nb_max=50)
+
+
+def test_check_idetical_field():
+    existing_fields = set()
+    existing_fields.add((('b_1', 6), ('b_2', 5), ('n_1', 3), ('n_2', 2), ('shape', 3)))
+    existing_fields.add((('b_1', 6), ('b_2', 5), ('n_1', 1), ('n_2', 2), ('shape', 3)))
+
+    assert not _check_identical_field({'shape': 3, 'n_1': 3, 'n_2': 2, 'b_1': 6, 'b_2': 5}, existing_fields)
+    assert not _check_identical_field({'shape': 3, 'n_1': 1, 'n_2': 2, 'b_1': 6, 'b_2': 5}, existing_fields)
+    assert _check_identical_field({'shape': 3, 'n_1': 3, 'n_2': 1, 'b_1': 6, 'b_2': 5}, existing_fields)
+    assert not _check_identical_field({'shape': 3, 'n_1': 2, 'n_2': 1, 'b_1': 5, 'b_2': 6}, existing_fields)
+    assert not _check_identical_field({'shape': 3, 'n_1': 2, 'n_2': 3, 'b_1': 5, 'b_2': 6}, existing_fields)
