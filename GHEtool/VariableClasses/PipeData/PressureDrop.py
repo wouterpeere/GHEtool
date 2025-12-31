87bd46fb4a4106395d24313c46cf1c2f8d38efe8
wouterpeere <wouter_peere@hotmail.com>
2025-12-31 09:49:00 +0100

    Add tests and increase coverage

--- GHEtool/VariableClasses/PipeData/PressureDrop.py
+++ GHEtool/VariableClasses/PipeData/PressureDrop.py
@@ -73,7 +73,8 @@ class PressureDrop:
         -------
         Pressure drop in kPa
         """
-        return self.pipe_data.pressure_drop(self.fluid_data, self.flow_data, self.borehole_length, **kwargs)
+        return self.pipe_data.pressure_drop(self.fluid_data, self.flow_data, self.borehole_length,
+                                            nb_of_boreholes=self.nb_of_boreholes, **kwargs)
 
     def calculate_pressure_drop_lateral(self, **kwargs) -> float:
         """
@@ -87,13 +88,14 @@ class PressureDrop:
 
         # Darcy fluid factor
         fd = gt.pipes.fluid_friction_factor_circular_pipe(
-            self.flow_data.mfr_borehole(fluid_data=self.fluid_data, **kwargs) * self.tichelmann_factor,
+            self.flow_data.mfr_borehole(fluid_data=self.fluid_data, nb_of_boreholes=self.nb_of_boreholes,
+                                        **kwargs) * self.tichelmann_factor,
             self.r_in_lateral,
             self.fluid_data.mu(**kwargs),
             self.fluid_data.rho(**kwargs),
             1e-6)
         A = pi * self.r_in_lateral ** 2
-        V = (self.flow_data.vfr_borehole(fluid_data=self.fluid_data,
+        V = (self.flow_data.vfr_borehole(fluid_data=self.fluid_data, nb_of_boreholes=self.nb_of_boreholes,
                                          **kwargs) / 1000) / A * self.tichelmann_factor
 
         # distance_later * 2 for back and forth
@@ -119,13 +121,11 @@ class PressureDrop:
             1e-6)
         A = pi * self.r_in_main ** 2
         V = (self.flow_data.vfr_borefield(fluid_data=self.fluid_data, nb_of_boreholes=self.nb_of_boreholes,
-                                          series_factor=self.series_factor,
-                                          **kwargs) / 1000) / A
+                                          series_factor=self.series_factor, **kwargs) / 1000) / A
 
         # distance_later * 2 for back and forth
         return ((fd * self.distance_main * 2 / (
-                2 * self.r_in_main) + self.minor_losses_main) * self.fluid_data.rho(
-            **kwargs) * V ** 2 / 2) / 1000
+                2 * self.r_in_main) + self.minor_losses_main) * self.fluid_data.rho(**kwargs) * V ** 2 / 2) / 1000
 
     def calculate_total_pressure_drop(self, **kwargs) -> float:
         """
@@ -158,7 +158,8 @@ class PressureDrop:
         # backup
         flow_backup = copy.copy(self.flow_data)
 
-        flow_rates = np.linspace(0, range * self.flow_data.vfr_borehole(fluid_data=self.fluid_data, **kwargs),
+        flow_rates = np.linspace(0, range * self.flow_data.vfr_borehole(fluid_data=self.fluid_data,
+                                                                        nb_of_boreholes=self.nb_of_boreholes, **kwargs),
                                  datapoints)
         pressure_drops_pipe = np.zeros(flow_rates.shape)
         pressure_drops_lateral = np.zeros(flow_rates.shape)
--- GHEtool/test/unit-tests/test_pressure_drop.py
+++ GHEtool/test/unit-tests/test_pressure_drop.py
@@ -27,12 +27,64 @@ def test_borehole():
                       pressure_drop_both.calculate_pressure_drop_borehole())
 
 
+def test_borehole_flow_borefield():
+    single_u = SingleUTube(1.5, 0.013, 0.016, 0.4, 0.035)
+    fluid_data = ConstantFluidData(0.568, 998, 4180, 1e-3)
+    flow_data = ConstantFlowRate(mfr=0.2 * 8, flow_per_borehole=False)
+    pressure_drop = PressureDrop(single_u, fluid_data, flow_data, 0, 100, 0.02 - 0.0037 / 2, 15, 0, 0.02 - 0.0037 / 2,
+                                 15, 0, 8, 1, 1)
+
+    pressure_drop_series = PressureDrop(single_u, fluid_data, flow_data,
+                                        0, 100, 0.02 - 0.0037 / 2, 15, 0, 0.02 - 0.0037 / 2, 15, 0, 8, 2, 1)
+    pressure_drop_tichelmann = PressureDrop(single_u, fluid_data, flow_data,
+                                            0, 100, 0.02 - 0.0037 / 2, 15, 0, 0.02 - 0.0037 / 2, 15, 0, 8, 1, 2)
+    pressure_drop_both = PressureDrop(single_u, fluid_data, flow_data,
+                                      0, 100, 0.02 - 0.0037 / 2, 15, 0, 0.02 - 0.0037 / 2, 15, 0, 8, 2, 2)
+
+    assert np.isclose(pressure_drop.calculate_pressure_drop_borehole(),
+                      pressure_drop_series.calculate_pressure_drop_borehole())
+    assert np.isclose(pressure_drop.calculate_pressure_drop_borehole(),
+                      pressure_drop_tichelmann.calculate_pressure_drop_borehole())
+    assert np.isclose(pressure_drop.calculate_pressure_drop_borehole(),
+                      pressure_drop_both.calculate_pressure_drop_borehole())
+
+
 def test_lateral():
     single_u = SingleUTube(1.5, 0.013, 0.016, 0.4, 0.035)
     fluid_data = ConstantFluidData(0.568, 998, 4180, 1e-3)
     flow_data = ConstantFlowRate(mfr=0.2)
     pressure_drop = PressureDrop(single_u, fluid_data, flow_data, 0, 100, 0.02 - 0.0037 / 2, 15, 0, 0, 0, 0, 8, 1, 1)
     pressure_drop_double = PressureDrop(single_u, fluid_data, ConstantFlowRate(mfr=0.4), 0, 100, 0.02 - 0.0037 / 2, 15,
+                                        0, 0, 0, 0, 8, 1, 1)
+
+    pressure_drop_series = PressureDrop(single_u, fluid_data, flow_data,
+                                        0, 100, 0.02 - 0.0037 / 2, 15, 0, 0, 0, 0, 8, 2, 1)
+    pressure_drop_tichelmann = PressureDrop(single_u, fluid_data, flow_data,
+                                            0, 100, 0.02 - 0.0037 / 2, 15, 0, 0, 0, 0, 8, 1, 2)
+    pressure_drop_both = PressureDrop(single_u, fluid_data, flow_data,
+                                      0, 100, 0.02 - 0.0037 / 2, 15, 0, 0, 0, 0, 8, 2, 2)
+
+    assert np.isclose(pressure_drop.calculate_pressure_drop_lateral(), 0.26307939880441045 * 2)
+    assert np.isclose(pressure_drop_series.calculate_pressure_drop_lateral(), 0.26307939880441045 * 2)
+    assert np.isclose(pressure_drop_tichelmann.calculate_pressure_drop_lateral(),
+                      pressure_drop_double.calculate_pressure_drop_lateral())
+    assert np.isclose(pressure_drop_both.calculate_pressure_drop_lateral(),
+                      pressure_drop_double.calculate_pressure_drop_lateral())
+
+    pressure_drop.minor_losses_lateral = 2
+    assert np.isclose(pressure_drop.calculate_pressure_drop_lateral(), 0.5635804695478487)
+
+    pressure_drop.fluid_data = TemperatureDependentFluidData('MPG', 25)
+    assert np.isclose(pressure_drop.calculate_pressure_drop_lateral(temperature=20), 0.7050447750402659)
+
+
+def test_lateral_flow_borefield():
+    single_u = SingleUTube(1.5, 0.013, 0.016, 0.4, 0.035)
+    fluid_data = ConstantFluidData(0.568, 998, 4180, 1e-3)
+    flow_data = ConstantFlowRate(mfr=0.2 * 8, flow_per_borehole=False)
+    pressure_drop = PressureDrop(single_u, fluid_data, flow_data, 0, 100, 0.02 - 0.0037 / 2, 15, 0, 0, 0, 0, 8, 1, 1)
+    pressure_drop_double = PressureDrop(single_u, fluid_data, ConstantFlowRate(mfr=0.4 * 8, flow_per_borehole=False), 0,
+                                        100, 0.02 - 0.0037 / 2, 15,
                                         0, 0, 0, 0, 8, 1, 1)
 
     pressure_drop_series = PressureDrop(single_u, fluid_data, flow_data,
@@ -94,10 +146,66 @@ def test_main_header():
                       pressure_drop_eight.calculate_pressure_drop_lateral(temperature=20))
 
 
+def test_main_header_flow_field():
+    single_u = SingleUTube(1.5, 0.013, 0.016, 0.4, 0.035)
+    fluid_data = ConstantFluidData(0.568, 998, 4180, 1e-3)
+    flow_data = ConstantFlowRate(mfr=0.2 * 8, flow_per_borehole=False)
+    pressure_drop = PressureDrop(single_u, fluid_data, flow_data, 0, 100, 0.02 - 0.0037 / 2, 15, 0, 0.02 - 0.0037 / 2,
+                                 15, 0, 8, 1, 1)
+    pressure_drop_eight = PressureDrop(single_u, fluid_data, ConstantFlowRate(mfr=1.6 * 8, flow_per_borehole=False), 0,
+                                       100, 0.02 - 0.0037 / 2, 15,
+                                       0, 0.02 - 0.0037 / 2, 15, 0, 8, 1, 1)
+    pressure_drop_four = PressureDrop(single_u, fluid_data, ConstantFlowRate(mfr=0.8 * 8, flow_per_borehole=False), 0,
+                                      100, 0.02 - 0.0037 / 2, 15,
+                                      0, 0.02 - 0.0037 / 2, 15, 0, 8, 1, 1)
+
+    pressure_drop_series = PressureDrop(single_u, fluid_data, ConstantFlowRate(mfr=0.2 * 4, flow_per_borehole=False),
+                                        0, 100, 0.02 - 0.0037 / 2, 15, 0, 0.02 - 0.0037 / 2, 15, 0, 8, 2, 1)
+    pressure_drop_tichelmann = PressureDrop(single_u, fluid_data, flow_data,
+                                            0, 100, 0.02 - 0.0037 / 2, 15, 0, 0.02 - 0.0037 / 2, 15, 0, 8, 1, 2)
+    pressure_drop_both = PressureDrop(single_u, fluid_data, ConstantFlowRate(mfr=0.2 * 4, flow_per_borehole=False),
+                                      0, 100, 0.02 - 0.0037 / 2, 15, 0, 0.02 - 0.0037 / 2, 15, 0, 8, 2, 2)
+
+    assert np.isclose(pressure_drop.calculate_pressure_drop_main(),
+                      pressure_drop_eight.calculate_pressure_drop_lateral())
+    assert np.isclose(pressure_drop_series.calculate_pressure_drop_main(),
+                      pressure_drop_four.calculate_pressure_drop_lateral())
+    assert np.isclose(pressure_drop_tichelmann.calculate_pressure_drop_main(),
+                      pressure_drop_eight.calculate_pressure_drop_lateral())
+    assert np.isclose(pressure_drop_both.calculate_pressure_drop_main(),
+                      pressure_drop_four.calculate_pressure_drop_lateral())
+
+    pressure_drop.minor_losses_main = 2
+    pressure_drop_eight.minor_losses_lateral = 2
+    assert np.isclose(pressure_drop.calculate_pressure_drop_main(),
+                      pressure_drop_eight.calculate_pressure_drop_lateral())
+
+    pressure_drop.fluid_data = TemperatureDependentFluidData('MPG', 25)
+    pressure_drop_eight.fluid_data = TemperatureDependentFluidData('MPG', 25)
+    assert np.isclose(pressure_drop.calculate_pressure_drop_main(temperature=20),
+                      pressure_drop_eight.calculate_pressure_drop_lateral(temperature=20))
+
+
 def test_total_pressure_drop():
     single_u = SingleUTube(1.5, 0.013, 0.016, 0.4, 0.035)
     fluid_data = ConstantFluidData(0.568, 998, 4180, 1e-3)
     flow_data = ConstantFlowRate(mfr=0.2)
+    pressure_drop = PressureDrop(single_u, fluid_data, flow_data, 0, 100, 0.02 - 0.0037 / 2, 15, 1, 0.02 - 0.0037 / 2,
+                                 15, 2, 8, 1, 1)
+    assert np.isclose(pressure_drop.calculate_total_pressure_drop(),
+                      pressure_drop.calculate_pressure_drop_borehole() + pressure_drop.calculate_pressure_drop_lateral()
+                      + pressure_drop.calculate_pressure_drop_main())
+    pressure_drop = PressureDrop(single_u, fluid_data, flow_data, 0, 100, 0.02 - 0.0037 / 2, 15, 1, 0.02 - 0.0037 / 2,
+                                 15, 2, 8, 2, 1)
+    assert np.isclose(pressure_drop.calculate_total_pressure_drop(),
+                      pressure_drop.calculate_pressure_drop_borehole() * 2 + pressure_drop.calculate_pressure_drop_lateral()
+                      + pressure_drop.calculate_pressure_drop_main())
+
+
+def test_total_pressure_drop_flow_borefield():
+    single_u = SingleUTube(1.5, 0.013, 0.016, 0.4, 0.035)
+    fluid_data = ConstantFluidData(0.568, 998, 4180, 1e-3)
+    flow_data = ConstantFlowRate(mfr=0.2 * 8, flow_per_borehole=False)
     pressure_drop = PressureDrop(single_u, fluid_data, flow_data, 0, 100, 0.02 - 0.0037 / 2, 15, 1, 0.02 - 0.0037 / 2,
                                  15, 2, 8, 1, 1)
     assert np.isclose(pressure_drop.calculate_total_pressure_drop(),
@@ -124,6 +232,28 @@ def test_range_pressure_drop():
         if idx == 0:
             continue
         pressure_drop = PressureDrop(single_u, fluid_data, ConstantFlowRate(vfr=val), 0, 100, 0.02 - 0.0037 / 2, 15, 1,
+                                     0.02 - 0.0037 / 2, 15, 2, 8, 1, 1)
+        assert np.isclose(pressure_drop.calculate_pressure_drop_borehole(temperature=20), pressure_pipe[idx])
+        assert np.isclose(pressure_drop.calculate_pressure_drop_lateral(temperature=20), pressure_lat[idx])
+        assert np.isclose(pressure_drop.calculate_pressure_drop_main(temperature=20), pressure_main[idx])
+
+
+def test_range_pressure_drop_borefield_flow():
+    single_u = SingleUTube(1.5, 0.013, 0.016, 0.4, 0.035)
+    flow_data = ConstantFlowRate(mfr=0.2 * 8, flow_per_borehole=False)
+    fluid_data = TemperatureDependentFluidData('Water', 0)
+    pressure_drop = PressureDrop(single_u, fluid_data, flow_data, 0, 100, 0.02 - 0.0037 / 2, 15, 1, 0.02 - 0.0037 / 2,
+                                 15, 2, 8, 1, 1)
+
+    pressure_pipe, pressure_lat, pressure_main, flow = pressure_drop.create_pressure_drop_curve(2, 30, temperature=20)
+    assert pressure_drop.flow_data == ConstantFlowRate(mfr=1.6, flow_per_borehole=False)
+
+    for idx, val in enumerate(flow):
+        if idx == 0:
+            continue
+            # flow is per borehole
+        pressure_drop = PressureDrop(single_u, fluid_data, ConstantFlowRate(vfr=val * 8, flow_per_borehole=False), 0,
+                                     100, 0.02 - 0.0037 / 2, 15, 1,
                                      0.02 - 0.0037 / 2, 15, 2, 8, 1, 1)
         assert np.isclose(pressure_drop.calculate_pressure_drop_borehole(temperature=20), pressure_pipe[idx])
         assert np.isclose(pressure_drop.calculate_pressure_drop_lateral(temperature=20), pressure_lat[idx])
